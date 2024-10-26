8305b775ba869caee7ba1a584e9e2a245f6a32d7
Nayan <nvbagale@gmail.com>
2024-10-26 17:11:05 +0530

    feat: Add constants for error messages
    Added a new file `messages.ts` under `apps/server/src/constants` to store error messages as constants. This will help in maintaining consistent error messages throughout the application.

---
+++ apps/server/src/constants/messages.ts
@@ -0,0 +1,17 @@
+const messages = {
+  REFRESH_TOKEN_EXPIRED: "Refresh token expired",
+  INVALID_REFRESH_TOKEN: "Invalid token",
+
+  ACCESS_TOKEN_EXPIRED: "Access token expired",
+  ACCESS_TOKEN_NOT_FOUND: "Access Token Not Found",
+  INVALID_ACCESS_TOKEN: "Invalid token",
+
+  UNAUTHORIED: "Unauthorized",
+
+  INVALID_CREDENTIALS: "Invalid credentials",
+  INTERNAL_SERVER_ERROR: "Internal server error",
+
+  LOGOUT: "Logged out",
+};
+
+export default messages;
