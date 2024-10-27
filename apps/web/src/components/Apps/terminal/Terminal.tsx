4ad09dc572dae59eef011556c8fc5b744734e435
Nayan <nvbagale@gmail.com>
2024-10-28 01:13:01 +0530

    feat: Add TerminalSkeleton component and Spinner UI
    - Add TerminalSkeleton component to display a loading spinner in the terminal area.
    - Create Spinner UI component to show a spinning icon for loading state.
    
    Refactor the Terminal component to import the Spinner component and use it in the TerminalSkeleton component.

--- apps/web/src/components/Apps/terminal/Terminal.tsx
+++ apps/web/src/components/Apps/terminal/Terminal.tsx
@@ -2,8 +2,19 @@ import { useDrag } from '@/components/hooks/useDrag';
 import { process } from '@/redux/features/apps/app/terminalSlice';
 import { useAppDispatch } from '@/redux/hooks';
 import { TerminalCard } from '@/ui/Cards/Terminal/TerminalCard';
+import Spinner from '@/ui/Spinner';
 // import { TerminalCard } from '@repo/ui';
 import { useRef } from 'react';
+
+export const TerminalSkeleton = () => {
+    return (
+        <div
+            className="absolute min-w-[32rem] min-h-[18rem] grid place-items-center z-20 text-gray-200 bg-black/60 backdrop-blur rounded-xl overflow-hidden"
+        >
+            <Spinner />
+        </div>
+    )
+}
 
 const Terminal = () => {
 
---
+++ apps/web/src/ui/Spinner.tsx
@@ -0,0 +1,11 @@
+import { Icons } from "@repo/ui/icons"
+
+const Spinner = () => {
+    return (
+        // <div className=" animate-spin">
+        <Icons.Spinner className=" animate-spin text-white h-10 w-10 bg-transparent " />
+        // </div>
+    )
+}
+
+export default Spinner
