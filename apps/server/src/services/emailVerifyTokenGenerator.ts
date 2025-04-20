ef61e862cfe7978421c69b1e955066af4da17e72
Nayan Bagale <nvbagale@gmail.com>
2025-04-20 19:56:29 +0530

    feat: update email verification URL generation to use SERVER_URL from environment variables

--- apps/server/src/services/emailVerifyTokenGenerator.ts
+++ apps/server/src/services/emailVerifyTokenGenerator.ts
@@ -2,10 +2,7 @@ import { LoginResponse } from "@skydock/types/Auth";
 import { createEmailVerificationToken } from "../utils/token";
 
 export const emailVerifyUrlGenerator = (user: LoginResponse) => {
-  const baseUrl =
-    process.env.NODE_ENV === "production"
-      ? process.env.FRONTEND_URL
-      : "http://localhost:3000";
+  const baseUrl = process.env.SERVER_URL || "http://localhost:3000";
   const token = createEmailVerificationToken(user);
   const url = `${baseUrl}/api/v1/auth/verify-email?token=${token}`;
   return url;
