d64badf1dd731abd4f0b80e2770ab2177b6cb596
Nayan Bagale <nvbagale@gmail.com>
2025-06-03 00:15:27 +0530

    feat: update background styling in useTheme hook for improved appearance

--- apps/web/src/components/hooks/useTheme.ts
+++ apps/web/src/components/hooks/useTheme.ts
@@ -21,6 +21,8 @@ const useTheme = () => {
       dispatch(changeBackground(background));
       document.body.style.backgroundImage = `url(${background?.src})`;
       document.body.style.backgroundRepeat = "no-repeat";
+      document.body.style.backgroundSize = "cover";
+      document.body.style.backgroundPosition = "center";
     }
   }, []);
 
