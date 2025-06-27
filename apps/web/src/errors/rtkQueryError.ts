b66fd5edee1b8339e097062ca155a2d0ffe5a0f4
Nayan Bagale <nvbagale@gmail.com>
2025-06-28 00:53:53 +0530

    refactor: Rename ErrorHandler to RtkErrorHandler and update error handling logic in useNotePad and store

--- apps/web/src/errors/rtkQueryError.ts
+++ apps/web/src/errors/rtkQueryError.ts
@@ -8,17 +8,19 @@ enum ErrorStatus {
   SERVER_ERROR = "SERVER_ERROR",
   UNEXPECTED_ERROR = "UNEXPECTED_ERROR",
   HANDLED_ERROR = "HANDLED_ERROR",
+  UNHANDLED_ERROR = "UNHANDLED_ERROR",
+  UNAUTHORIZED = 401,
 }
 
-export class ErrorHandler {
-  private static toast = (message: string) => {
+export class RtkErrorHandler {
+  protected static toast = (message: string) => {
     return showToast(message, "error");
   };
 
   public static rtkQueryErrorMiddleware: Middleware =
     (api: MiddlewareAPI) => (next) => (action) => {
       if (isRejectedWithValue(action)) {
-        console.error("RTK Query Error:", action);
+        // console.error("RTK Query Error:", action);
         const status =
           action.payload &&
           typeof action.payload === "object" &&
@@ -28,41 +30,65 @@ export class ErrorHandler {
 
         if (status === ErrorStatus.FETCH_ERROR) {
           this.toast("Network error. Please check your connection.");
+          (action.payload as { handled?: string }).handled =
+            ErrorStatus.HANDLED_ERROR;
         } else if (status === ErrorStatus.PARSING_ERROR) {
           this.toast("Data parsing error. Please try again.");
+          (action.payload as { handled?: string }).handled =
+            ErrorStatus.HANDLED_ERROR;
         } else if (status === ErrorStatus.SERVER_ERROR) {
           this.toast("Server error. Please try again later.");
-        } else {
-          this.toast("An unexpected error occurred. Please try again.");
-        }
-
-        if (
-          action.payload &&
-          typeof action.payload === "object" &&
-          "status" in action.payload
-        ) {
-          (action.payload as { status?: string }).status =
+          (action.payload as { handled?: string }).handled =
+            ErrorStatus.HANDLED_ERROR;
+        } else if (Number(status) === ErrorStatus.UNAUTHORIZED) {
+          console.warn("Unauthorized access attempt detected.");
+          (action.payload as { handled?: string }).handled =
             ErrorStatus.HANDLED_ERROR;
         }
       }
+
       return next(action);
     };
+}
 
-  public static handleError(error: unknown, message: string): void {
-    if (error instanceof Error) {
-      this.toast(error.message);
-    } else if (typeof error === "string") {
-      this.toast(error);
-    } else if (
+export class ErrorHandler extends RtkErrorHandler {
+  private hasDisplayed = false;
+
+  constructor(error: unknown) {
+    super();
+    this.handleError(error);
+  }
+
+  public handleError(error: unknown): ErrorHandler {
+    console.log(error);
+    if (
       typeof error === "object" &&
       error !== null &&
-      "status" in error &&
-      (error as { status?: string }).status === ErrorStatus.HANDLED_ERROR
+      "handled" in error &&
+      (error as { handled?: string }).handled === ErrorStatus.HANDLED_ERROR
     ) {
-      console.warn("Handled error:", message);
-    } else {
-      this.toast("An unexpected error occurred.");
+      this.hasDisplayed = true;
+    } else if (error instanceof Error) {
+      this.internalDisplay(error.message);
+      this.hasDisplayed = true;
     }
-    console.error("Error:", error);
+    return this;
+  }
+
+  private internalDisplay(message: string): void {
+    this.hasDisplayed = true;
+    ErrorHandler.toast(message);
+  }
+
+  public display(message: string): this {
+    if (!this.hasDisplayed) {
+      this.hasDisplayed = true;
+      ErrorHandler.toast(message);
+    }
+    return this;
   }
 }
+
+export const HandleError = (error: unknown) => {
+  return new ErrorHandler(error);
+};
--- apps/web/src/redux/store.ts
+++ apps/web/src/redux/store.ts
@@ -1,4 +1,4 @@
-import { ErrorHandler } from "@/errors/rtkQueryError";
+import { RtkErrorHandler } from "@/errors/rtkQueryError";
 import authReducer from "@/redux/features/auth";
 import explorerReducer from "@/redux/features/explorer/explorerSlice";
 import { configureStore } from "@reduxjs/toolkit";
@@ -46,7 +46,7 @@ export const store = configureStore({
       .concat(userAuthApi.middleware)
       .concat(filesAndFolderApi.middleware)
       .concat(planApi.middleware)
-      .concat(ErrorHandler.rtkQueryErrorMiddleware),
+      .concat(RtkErrorHandler.rtkQueryErrorMiddleware),
   devTools: process.env.NODE_ENV !== "prod",
 });
 
