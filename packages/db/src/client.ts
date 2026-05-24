bab3d7a73e06a33863bbd1350d00b281254c5f0c
Nayan Bagale <nayanbagale@Nayans-MacBook-Air.local>
2026-05-25 00:35:03 +0530

    chore:upgraded-low-medium-libraries-node-24

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,9 +1,37 @@
+import "dotenv/config";
+
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const databaseUrl = process.env.DATABASE_URL;
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
+const adapter = new PrismaPg({
+  connectionString: databaseUrl,
+});
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ??
+  new PrismaClient({
+    adapter,
+
+    // optional query logs
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
 
 export type { PrismaClient };
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
